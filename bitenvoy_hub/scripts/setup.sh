b23aae2158b56b028b6ed23ba69c1331d42b9aba
unspar <tkamitaki@gmail.com>
2016-10-01 22:04:52 -0400

    fixed setup per readme

--- bitenvoy_hub/scripts/setup.sh
+++ bitenvoy_hub/scripts/setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
+cd ..
 #5432 is default postgres port, it must be exposed to function 
-sudo docker run -d -p 5432:5432 postgres
+sudo docker run --name postgres -d -p 5432:5432 postgres
+mix ecto.create && ecto.migrate
+npm install
 
 
 #create database
 psql -h localhost -U postgres -c 'CREATE DATABASE bitenvoy_hub_dev;'
 
+mix phoenix.server
 
 
 
